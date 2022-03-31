8187a3a579eda64ef77cf92a0e974fb9ad2147c5
fendyIskandar <fendyaremania87@gmail.com>
2022-03-31 11:18:05 -0700

    25.15

---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+Widget brandName() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: <Widget>[
+      Text(
+        "Wallpaper",
+        style: TextStyle(color: Colors.black87, fontFamily: 'Overpass'),
+      ),
+      Text(
+        "Hub",
+        style: TextStyle(color: Colors.white, fontFamily: 'Overpass'),
+      ),
+    ],
+  );
+}
