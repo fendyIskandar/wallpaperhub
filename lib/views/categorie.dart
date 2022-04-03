8d007b61a6e468e34b5804f12c8a985efafad639
fendyIskandar <fendyaremania87@gmail.com>
2022-04-03 10:09:03 -0700

    Search 1.52

--- lib/views/categorie.dart
+++ lib/views/categorie.dart
@@ -1,1 +1,45 @@
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:wallpaperhub/data/data.dart';
+import 'package:wallpaperhub/model/wallpaper_model.dart';
+
+class Categorie extends StatefulWidget {
+  @override
+  _CategorieState createState() => _CategorieState();
+}
+
+class _CategorieState extends State<Categorie> {
+  List<WallpaperModel> wallpapers = [];
+
+  getSearchWallpapers(String query) async {
+    var response = await http.get(
+        Uri.parse(
+            "https://api.pexels.com/v1/search?query=$query&per_page=15&page=1"),
+        headers: {"Authorization": apikey});
+
+    //print(response.body.toString());
+
+    Map<String, dynamic> jsonData = jsonDecode(response.body);
+    jsonData["photos"].forEach((element) {
+      //print(element);
+      WallpaperModel wallpaperModel = new WallpaperModel();
+      wallpaperModel = WallpaperModel.fromMap(element);
+      wallpapers.add(wallpaperModel);
+    });
+
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
